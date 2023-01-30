a3e76bb5f721b3738a69cb41ebcc7ebd93ab0220
Bridget McGivern <95941779+bmcgivern13@users.noreply.github.com>
2023-01-30 16:58:17 -0700

    Update 4.0_figure_S5.R

--- figures/4.0_figure_S5.R
+++ figures/4.0_figure_S5.R
@@ -1,1 +1,21 @@
-metabolite heatmap
+library(readxl)
+library(dplyr)
+library(tidyr)
+library(pheatmap)
+
+set.seed(08211995)
+
+# read in Data_S4
+data = read_excel("Data_S4.xlsx",sheet="polyphenols_phenolics")
+
+# find average value of each compound per habitat and depth compartment
+ave_by_cmpart=data%>%select(-method)%>%gather(-name,key="sample",value="area")%>%
+  mutate(compartment=paste(substr(sample,1,1),substr(sample,3,3),sep=""))%>%
+  group_by(name,compartment)%>%summarise(ave=mean(area))%>%
+  spread(key = name,value=ave)
+ave_by_cmpart=as.data.frame(ave_by_cmpart)
+row.names(ave_by_cmpart)=ave_by_cmpart[,1]
+ave_by_cmpart=ave_by_cmpart[,-1]
+
+# plot heatmap to be used in Fig. S5
+pheatmap(ave_by_cmpart,scale="column",cluster_rows = FALSE)
