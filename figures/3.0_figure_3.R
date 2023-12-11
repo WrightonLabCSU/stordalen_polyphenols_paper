8497bca7ce86b96a90d9f5894835e5018f36ae12
Bridget McGivern <95941779+bmcgivern13@users.noreply.github.com>
2023-12-11 10:36:27 -0700

    Update 3.0_figure_3.R

--- figures/3.0_figure_3.R
+++ figures/3.0_figure_3.R
@@ -24,19 +24,19 @@ colnames(annotation)=c("MAG","tax")
 annotation=annotation%>%separate(tax,into=c("d","p","c","o","f","g","s"),sep=";",remove=FALSE)
 
 # read in phylum color codes
-p_colors=read_excel("Supplementary_Table_3.xlsx",sheet = "color_codes")
+p_colors=read_excel("Supplementary_Data_3.xlsx",sheet = "color_codes")
 
 # read in mag metaG data
-mag_encoded_paths=read_excel("Supplementary_Table_2.xlsx",sheet="MAG_metaG_paths")
+mag_encoded_paths=read_excel("Supplementary_Data_2.xlsx",sheet="MAG_metaG_paths")
 mag_encoded_paths= mag_encoded_paths %>%
   select(mag,path,presence)
 mag_encoded_paths$pathway=mag_encoded_paths$path
-
+tal_dom=read_excel("tal_dom.xlsx")
 # read in and create MAG metaT data
-pp_metaT_mags = read_excel("Supplementary_Table_2.xlsx",sheet="MAG_metaT_polyphenol_genes")
-info = read_excel("Supplementary_Table_2.xlsx",sheet="polyphenol_genes")
+pp_metaT_mags = read_excel("Supplementary_Data_2.xlsx",sheet="MAG_metaT_polyphenol_genes")
+info = read_excel("Supplementary_Data_2.xlsx",sheet="polyphenol_genes")
 info= info %>%select(gene_id,gene_description,Transformation,Substrate,oxygen)
-metaT_genes=inner_join(info,pp_metaT_mags,by=c("gene_id"="ID"))
+metaT_genes=inner_join(info,pp_metaT_mags,by=c("gene_id"="ID"),relationship = "many-to-many")
 
 mag_metaT_paths=inner_join(metaT_genes,mag_encoded_paths,by=c("MAG"="mag","Transformation"="path"))
 
@@ -51,12 +51,13 @@ mag_activity=mag_metaT_paths%>%
 mag_activity%>%ungroup()%>%group_by(MAG)%>%summarise(sum=sum(pa))%>%ungroup()%>%group_by(sum)%>%summarise(n=n())
 
 # get number of pathways encoded per MAG
-paths=read_excel("Supplementary_Table_2.xlsx", sheet="MAG_metaG_paths")
+paths=read_excel("Supplementary_Data_2.xlsx", sheet="MAG_metaG_paths")
 paths_summary = paths %>%
   select(mag,path,family,presence)%>%
   distinct()%>%
   group_by(mag,family)%>%
   summarise(count=sum(presence))
+tal_dom=read.delim("4.2_talent_dominant.txt",sep="\t")
 
 # make bacterial tree
 bac_dat=as.data.frame(bac_tree$tip.label)
@@ -69,13 +70,11 @@ colnames(bac_fill)=c("p")
 bac_fill=left_join(bac_fill,bac_p_fill,by="p")
 mag_activity$site=factor(mag_activity$site,levels = c("P","S","E"))
 
-#manually pull nodes for key genera
+#pull nodes for key genera
 bac_parents=as_tibble(bac_tree)
-nodeids=c(3111,2901,2862,2133,2319,3348,1534,3251,3239,3457,3484,3283,3276)
-nodedf=data.frame(node=nodeids)
-nodelab <- c("Terra","Palsa-89","Palsa-295","Palsa-504","Lustribacter","Palsa-883","UBA6159","Roseiarcus","Novosphing","Bog-1198","FEN-1197","Bog-931","Bradyrhiz")
-poslist=c(1)
-labdf <- data.frame(node=nodeids, label=nodelab, pos=poslist)
+bac_tal_dom_nodes=bac_parents%>%mutate(label=gsub(":g__","=g__",label))%>%mutate(label=gsub("; g__","=g__",label))%>%separate(label,into=c("one","g"),sep="=")%>%filter(is.na(g)==0)%>%mutate(g=gsub("'","",g))%>%right_join(.,annotation,by=c("g"))%>%select(-MAG,-tax,-s,-one,-branch.length)%>%distinct()%>%mutate(genus=paste(d,p,c,o,f,g,sep=";"))%>%right_join(.,tal_dom,by="genus")%>%select(node,genus)%>%mutate(pos=1)
+labdf=bac_tal_dom_nodes
+colnames(labdf)=c("node","label","pos")
 
 # undecorated tree
 p=ggtree(bac_tree, layout="fan", branch.length = "branch.length", size=0.2,open.angle=50) 
@@ -95,33 +94,59 @@ bac=p+
   geom_fruit(data=mag_activity,
              geom=geom_point,
              mapping=aes(y=MAG,x=site,size=pa/3,color=site),offset=0.01,grid.params = list(),axis.params = list(axis="x"))+
-  geom_highlight(node=3111,fill="orange")+
-  geom_highlight(node=2901,fill="red")+
-  geom_highlight(node=2862,fill="orange")+
-  geom_highlight(node=2133,fill="red")+
-  geom_highlight(node=2319,fill="red")+
-  geom_highlight(node=3348,fill="red")+
-  geom_highlight(node=1534,fill="red")+
-  geom_highlight(node=3251,fill="orange")+
-  geom_highlight(node=3239,fill="red")+
-  geom_highlight(node=3457,fill="red")+
-  geom_highlight(node=3484,fill="red")+
-  geom_highlight(node=3283,fill="red")+
-  geom_highlight(node=3276,fill="red")+
-  geom_highlight(node=2954,fill="yellow")+
-  geom_highlight(node=2974,fill="yellow")+
-  geom_highlight(node=2999,fill="yellow")+
-  geom_highlight(node=3330,fill="yellow")+
-  geom_highlight(node=3269,fill="yellow")+
-  geom_highlight(node=2455,fill="yellow")+
-  geom_highlight(node=3050,fill="yellow")+
-  geom_highlight(node=3261,fill="yellow")+
-  geom_highlight(node=3425,fill="yellow")+
-  geom_highlight(node=2915,fill="yellow")+
-  geom_highlight(node=2216,fill="yellow")+
-  geom_highlight(node=2024,fill="yellow")+
-  geom_highlight(node=3547,fill="yellow")+
-  geom_highlight(node=3366,fill="yellow")
+  geom_hilight(node=2024,fill="yellow")+ #g__Fen-1064, dominant
+  geom_hilight(node=2036,fill="red")+ #g__Palsa-465, talent
+  geom_hilight(node=2133,fill="orange")+ #g__Palsa-504
+  geom_hilight(node=2197,fill="yellow")+ #g__Bog-473
+  geom_hilight(node=2216,fill="yellow")+ #g__RAAP-2
+  geom_hilight(node=2266,color="yellow")+ #g__Fen-455
+  geom_hilight(node=2319,fill="red")+ #g__Lustribacter
+  geom_hilight(node=2336,fill="yellow")+ #g__Palsibacter
+  geom_hilight(node=2424,fill="yellow")+ #g__Palsa-1400
+  geom_hilight(node=2455,fill="yellow")+ #g__UBA11358
+  geom_hilight(node=2615,fill="yellow")+ #g__ELB16-189
+  geom_hilight(node=2656,fill="yellow")+ #g__LD21
+  geom_hilight(node=2686,fill="yellow")+ #g__Paludibacter
+  geom_hilight(node=2862,fill="orange")+ #g__Palsa-295
+  geom_hilight(node=2893,fill="yellow")+ #g__Palsa-187
+  geom_hilight(node=2901,fill="red")+ #g__Palsa-89
+  geom_hilight(node=2907,fill="yellow")+ #g__Bog-105
+  geom_hilight(node=2915,fill="yellow")+ #g__Bog-159
+  geom_hilight(node=2954,fill="yellow")+ #g__BOG-234
+  geom_hilight(node=2974,fill="yellow")+ #g__Bog-209
+  geom_hilight(node=2999,fill="yellow")+ #g__Sulfotelmatobacter
+  geom_hilight(node=3050,fill="yellow")+ #g__TOLSYN
+  geom_hilight(node=3104,fill="yellow")+ #g__CAIVDV01
+  geom_hilight(node=3111,fill="orange")+ #g__Terracidiphilus
+  geom_hilight(node=3230,fill="yellow")+ #g__2-02-FULL-66-22
+  geom_hilight(node=1513,fill="yellow")+#f__Magnetospirillaceae;g__
+  geom_hilight(node=3239,fill="red")+ #g__Novosphingobium
+  geom_hilight(node=1533,fill="red")+ #g__BOG-934
+  geom_hilight(node=1532,fill="red")+ #g__Rhodopila
+  geom_hilight(node=1534,fill="red")+ #g__UBA6159
+  geom_hilight(node=3251,fill="red")+ #g__Roseiarcus
+  geom_hilight(node=3261,fill="yellow")+ #g__Methylocella
+  geom_hilight(node=3264,fill="red")+ #g__Rhodoblastus
+  geom_hilight(node=3269,fill="yellow")+ #g__Methylocystis
+  geom_hilight(node=3276,fill="red")+ #g__Bradyrhizobium
+  geom_hilight(node=3283,fill="red")+ #g__BOG-931
+  geom_hilight(node=3330,fill="orange")+ #g__Acidocella
+  geom_hilight(node=3348,fill="red")+ #g__Palsa-883
+  geom_hilight(node=3361,fill="red")+ #g__AP-15
+  geom_hilight(node=3366,fill="yellow")+ #g__BOG-933
+  geom_hilight(node=3389,fill="yellow")+ #g__LMDS01
+  geom_hilight(node=3413,fill="yellow")+ #g__2-12-FULL-64-23
+  geom_hilight(node=3425,fill="yellow")+ #g__PALSA-1006
+  geom_hilight(node=3457,fill="orange")+ #g__Bog-1198
+  geom_hilight(node=3484,fill="orange")+ #g__FEN-1191
+  geom_hilight(node=3586,fill="yellow")+ #g__PMG-095
+  geom_hilight(node=3591,fill="yellow")+ #g__Fen-1137
+  geom_hilight(node=468,color="yellow")+ #g__PALSA-693 actino
+  geom_hilight(node=194,fill="yellow")+ #f__Ktedonobacteraceae;g__
+  geom_hilight(node=1735,fill="yellow")+ #g__Fen-1087
+  geom_hilight(node=3544,fill="yellow")+ #g__Fen-1135
+  geom_hilight(node=3550,fill="yellow")+ #g__PNOF01
+  geom_hilight(node=1049,color="yellow") #f__UBA7541;g__
 
 # make archaeal tree
 arc_dat=as.data.frame(arc_tree$tip.label)
@@ -133,6 +158,12 @@ arc_p_fill=left_join(arc_dat,p_colors,by=c("p"="phylum"))%>%select(p,code)%>%dis
 arc_fill=as.data.frame(sort(arc_p_fill$p))
 colnames(arc_fill)=c("p")
 arc_fill=left_join(arc_fill,arc_p_fill,by="p")
+
+#pull nodes for key genera
+arc_parents=as_tibble(arc_tree)
+arc_tal_dom_nodes=arc_parents%>%mutate(label=gsub(":g__","=g__",label))%>%mutate(label=gsub("; g__","=g__",label))%>%separate(label,into=c("one","g"),sep="=")%>%filter(is.na(g)==0)%>%mutate(g=gsub("'","",g))%>%right_join(.,annotation,by=c("g"))%>%select(-MAG,-tax,-s,-one,-branch.length)%>%distinct()%>%mutate(genus=paste(d,p,c,o,f,g,sep=";"))%>%right_join(.,tal_dom,by="genus")%>%select(node,genus)%>%mutate(pos=1)
+labdf=arc_tal_dom_nodes
+colnames(labdf)=c("node","label","pos")
 
 # undecorated tree
 a=ggtree(arc_tree, layout="fan", branch.length = "branch.length", size=0.2,open.angle=350) 
@@ -153,6 +184,9 @@ arc=a+geom_fruit(data=arc_dat,geom=geom_tile,
   geom_fruit(data=mag_activity,
              geom=geom_point,
              mapping=aes(y=MAG,x=site,size=pa/3,color=site),offset=0.1,grid.params = list(),axis.params = list(axis="x"))+
-  geom_highlight(node=90,fill="yellow")
+  geom_highlight(node=90,fill="yellow")+ #g__Bog-38
+  geom_highlight(node=40,fill="yellow")+ #g__Fen-7
+  geom_highlight(node=95,fill="yellow")+ #g__Methanosarcina
+  geom_highlight(node=98,fill="yellow") #g__Methanothrix
 
 plot_grid(bac,arc)
