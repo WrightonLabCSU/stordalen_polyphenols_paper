ba943fa7cbd4e720aa056960c4ac6e6740183f16
Bridget McGivern <95941779+bmcgivern13@users.noreply.github.com>
2023-12-11 10:32:23 -0700

    Update 4.0_MAG_metaT_polyphenol_curation.R

--- data_processing/4.0_MAG_metaT_polyphenol_curation.R
+++ data_processing/4.0_MAG_metaT_polyphenol_curation.R
@@ -1,22 +1,24 @@
 library(readxl)
 library(dplyr)
 library(tidyr)
+library(cowplot)
 library(writexl)
+library(ggridges)
 
 set.seed(08211995)
 
 #read in mag-level metaT abundance data
-mag_geTMM = read_excel("Supplementary_Table_3.xlsx",sheet="metaT_MAGs")
+mag_geTMM = read_excel("Supplementary_Data_3.xlsx",sheet="metaT_MAGs")
 # read in gtdb v2
-tax = read_excel("Supplementary_Table_3.xlsx",sheet="MAGs")
+tax = read_excel("Supplementary_Data_3.xlsx",sheet="MAGs")
 tax=tax%>%select(MAG,GTDB_v2_r207)
 # join metaT abundance with taxonomy
 mag_geTMM=left_join(mag_geTMM,tax,by="MAG")
 # read in phlyum color codes
-phylum=read_excel("Supplementary_Table_3.xlsx",sheet="color_codes")
+phylum=read_excel("Supplementary_Data_3.xlsx",sheet="color_codes")
 
 # read in MAG metaG abundance
-abund = read_excel("Supplementary_Table_3.xlsx",sheet="metaG_MAGs")
+abund = read_excel("Supplementary_Data_3.xlsx",sheet="metaG_MAGs")
 # convert metaG abundance to tidy format
 tidy_abund = abund %>%
   gather(-MAG,key="sample",value="tpm")
@@ -28,7 +30,7 @@ ave_tidy_abund = tidy_abund %>%
   summarise(ave=mean(tpm))
 
 # read in MAG encoded transformations
-mag_encoded_paths=read_excel("Supplementary_Table_2.xlsx",sheet="MAG_metaG_paths")
+mag_encoded_paths=read_excel("Supplementary_Data_2.xlsx",sheet="MAG_metaG_paths")
 mag_encoded_paths= mag_encoded_paths %>%
   select(mag,path,presence)
 #mag_encoded_paths$MAG_ID=paste(mag_encoded_paths$taxonomy,mag_encoded_paths$mag,sep=";")
@@ -36,10 +38,10 @@ mag_encoded_paths$pathway=mag_encoded_paths$path
 #mag_encoded_paths= mag_encoded_paths %>%
 #select(-family.x,-taxonomy)
 
-pp_metaT_mags = read_excel("Supplementary_Table_2.xlsx",sheet="MAG_metaT_polyphenol_genes")
-info = read_excel("Supplementary_Table_2.xlsx",sheet="polyphenol_genes")
+pp_metaT_mags = read_excel("Supplementary_Data_2.xlsx",sheet="MAG_metaT_polyphenol_genes")
+info = read_excel("Supplementary_Data_2.xlsx",sheet="polyphenol_genes")
 info= info %>%select(gene_id,gene_description,Transformation,Substrate,oxygen)
-metaT_genes=inner_join(info,pp_metaT_mags,by=c("gene_id"="ID"))
+metaT_genes=inner_join(info,pp_metaT_mags,by=c("gene_id"="ID"),relationship = "many-to-many")
 tidy_metaT_genes = metaT_genes %>%
   select(Transformation,Substrate,oxygen,MAG,sample,geTMM,site,depth) %>%
   group_by(Transformation,Substrate,oxygen,MAG,geTMM,site,depth) %>%
@@ -50,11 +52,11 @@ tidy_metaT_paths = tidy_metaT_genes %>%
   summarise(sum=sum(ave))
 
 #combine metaG potential with metaT expression
-data1 = left_join(ave_tidy_abund,mag_encoded_paths,by=c("MAG"="mag"))
+data1 = left_join(ave_tidy_abund,mag_encoded_paths,by=c("MAG"="mag"),relationship = "many-to-many")
 #add metaT 
 data1$path=tolower(data1$path)
 tidy_metaT_paths$Transformation=tolower(tidy_metaT_paths$Transformation)
-data2 = left_join(data1,tidy_metaT_paths,by=c("MAG","site","depth","path"="Transformation"))
+data2 = left_join(data1,tidy_metaT_paths,by=c("MAG","site","depth","path"="Transformation"),relationship = "many-to-many")
 data3 = data2 %>% select(-path,-Substrate,-oxygen)
 # add column about whether it's encoded in the MAG
 data3$metaG=ifelse(data3$ave>0,ifelse(is.na(data3$presence)==0,"metaG","not"),"absent")
@@ -103,7 +105,7 @@ max=mag_metaT_paths %>%
   group_by(Transformation,Substrate,oxygen,sample) %>%
   summarise(sum=sum(geTMM)) %>%
   summarise(max=max(sum))
-paths = mag_metaT_paths %>%
+paths1 = mag_metaT_paths %>%
   ungroup() %>%
   group_by(Transformation,Substrate,oxygen,sample) %>%
   summarise(sum=sum(geTMM)) %>%
@@ -113,18 +115,54 @@ paths = mag_metaT_paths %>%
   ungroup()%>%
   select(-Substrate,-oxygen) %>%
   spread(key=sample,value=rel)
-paths[is.na(paths)] <- 0
+paths1[is.na(paths1)] <- 0
+
+ave_paths1 = paths1%>%
+  gather(-Transformation,key="sample",value="sum")%>%
+  mutate(compartment=paste(substr(sample,1,1),substr(sample,3,3),sep=""))%>%
+  group_by(Transformation,compartment)%>%summarise(ave=mean(sum))%>%
+  spread(key=compartment,value=ave)
+
+ave_paths1[is.na(ave_paths1)] <- 0
+ave_paths1=as.data.frame(ave_paths1)
+row.names(ave_paths1)=ave_paths1[,1]
+ave_paths1=ave_paths1[,-1]
+
+pheatmap(t(ave_paths1),cluster_rows = FALSE,cluster_cols=TRUE,scale="column")
+
+
+paths2 = mag_metaT_paths %>%
+  ungroup() %>%
+  group_by(Transformation,Substrate,oxygen,sample) %>%
+  summarise(sum=sum(geTMM)) %>%
+   ungroup()%>%
+  select(-Substrate,-oxygen) %>%
+  spread(key=sample,value=sum)
+paths2[is.na(paths2)] <- 0
+
+ave_paths2 = paths2%>%
+  gather(-Transformation,key="sample",value="sum")%>%
+  mutate(compartment=paste(substr(sample,1,1),substr(sample,3,3),sep=""))%>%
+  group_by(Transformation,compartment)%>%summarise(ave=mean(sum))%>%
+  spread(key=compartment,value=ave)
+
+ave_paths2[is.na(ave_paths2)] <- 0
+ave_paths2=as.data.frame(ave_paths2)
+row.names(ave_paths2)=ave_paths2[,1]
+ave_paths2=ave_paths2[,-1]
+
+pheatmap(t(ave_paths2),cluster_rows = FALSE,cluster_cols=TRUE,scale="column")
+
 
 # write table 
 write_xlsx(paths,"pathway_table_relabun.xlsx")
-# using Supplementary_Table_2 tab polyphenol_genes, figure_2_rxn_number column, remove duplicate entries
-# this will now be Supplementary_Table_2 tab "metaT_pathway_relabun"
+# using Supplementary_Data_2 tab polyphenol_genes, figure_2_rxn_number column, remove duplicate entries
+# this will now be Supplementary_Data_2 tab "metaT_pathway_relabun"
 
-
-##########
-# calculating talent
-##########
 g_pathways=mag_metaT_paths%>%left_join(.,tax,by="MAG")%>%separate(GTDB_v2_r207,into=c("d","p","c","o","f","g","s"),sep=";")%>%mutate(genus=paste(d,p,c,o,f,g,sep=";"))%>%filter(geTMM>0)%>%select(genus,site,pathway)%>%distinct()%>%group_by(genus,site)%>%summarise(n=n())
+
+mag_metaT_paths%>%left_join(.,tax,by="MAG")%>%separate(GTDB_v2_r207,into=c("d","p","c","o","f","g","s"),sep=";")%>%mutate(genus=paste(d,p,c,o,f,g,sep=";"))%>%filter(geTMM>0)%>%select(genus,site,pathway)%>%filter()
+
 
 # figuring out the 95% threshold for pathways expressed (talent cut-off)
 quantile(g_pathways$n, probs = 0.95)  
@@ -133,19 +171,19 @@ median(g_pathways$n) #4
 sd(g_pathways$n)
 density(g_pathways$n)
 
+
 a=ggplot(g_pathways, aes(x = n, y = 1, fill = (0.5 - abs(0.5 - stat(ecdf)))<0.05)) +
   stat_density_ridges(geom = "density_ridges_gradient", calc_ecdf = TRUE) +
   scale_fill_discrete(name = "Tail probability <0.05")+
   geom_vline(xintercept = 15)+
   theme_classic()+xlab("Unique Pathways Expressed")+ylab("")
 
-##########
-# calculating dominance
-##########
-category=read_delim("4.1_pathway_categories.txt",sep="\t")
+category=read_excel("../../../project_data/metaT/mapped_97perMAGs/metaT_adj_camper.xlsx",sheet="categories")
+
 
 # removing false positive annotations
 mag_metaT_paths2=mag_metaT_paths%>%filter(gene!="3300037104_19_Ga0395706_017518_1")%>%filter(gene!="3300037104_19_Ga0395706_013576_5")%>%filter(gene!="3300037104_19_Ga0395706_002833_4")%>%filter(gene!="3300037104_19_Ga0395706_002118_12")%>%filter(gene!="20110800_E1S_4_c_000000002448_2")%>%filter(gene!="20120700_S1D_59_c_000000099081_15")%>%filter(gene!="3300037104_19_Ga0395706_001630_7")%>%filter(gene!="PLGY01_ENA|PLGY01000051|PLGY01000051.1_69")%>%filter(gene!="PMEE01_ENA|PMEE01000053|PMEE01000053.1_14")%>%filter(gene!="PMNG01_ENA|PMNG01000155|PMNG01000155.1_37")%>%filter(gene!="PMNG01_ENA|PMNG01000199|PMNG01000199.1_11")%>%filter(gene!="3300037104_19_Ga0395706_002511_1")%>%filter(gene!="20120800_E2X_6_c_000000002298_6")%>%filter(gene!="20120800_E2X_6_c_000000002427_2")%>%filter(gene!="20150700_E25_9_c_000000001274_3")%>%filter(gene!="20150700_E25_9_c_000000001337_8")
+
 total=mag_metaT_paths2%>%left_join(.,tax,by="MAG")%>%separate(GTDB_v2_r207,into=c("d","p","c","o","f","g","s"),sep=";")%>%mutate(genus=paste(d,p,c,o,f,g,sep=";"))%>%filter(geTMM>0)%>%select(genus,sample,site,depth,pathway,geTMM,Substrate)%>%distinct()%>%group_by(genus,sample,site,depth,pathway,Substrate)%>%summarise(sum=sum(geTMM))%>%left_join(.,category,by="pathway")%>%filter(is.na(group)==0)%>%ungroup()%>%distinct()%>%group_by(sample,site,depth,group)%>%summarise(total=sum(sum))
 
 dom=mag_metaT_paths2%>%
@@ -161,18 +199,9 @@ b=ggplot(dom, aes(x = ave_abun*100, y = 1, fill = (0.5 - abs(0.5 - stat(ecdf)))<
   geom_vline(xintercept = 10)+
   theme_classic()+xlab("Relative Contribution to Community Polymer, Monomer, or Phenolic/Benzoic Expression (%)")+ylab("")
 
-
-# making som figure 
 plot_grid(a,b,ncol = 1)
-
 
 dom_pass=dom%>%filter(ave_abun>=0.10)%>%ungroup()%>%select(genus)%>%distinct()%>%mutate(dominant="y")
 tal_pass=g_pathways%>%filter(n>=15)%>%ungroup()%>%select(genus)%>%distinct()%>%mutate(talent="y")
 tal_dom=full_join(dom_pass,tal_pass,by="genus")
 write.table(tal_dom,"4.2_talent_dominant.txt", sep="\t")
-
-
-
-
-
-
